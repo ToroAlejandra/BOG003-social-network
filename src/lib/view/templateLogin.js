49d680eeffcbae682252de8678958c74d467bf62
TefyRabih <tefyrabih@gmail.com>
2021-08-17 15:27:45 -0500

    inicio Ruteo

---
+++ src/lib/view/templateLogin.js
@@ -0,0 +1,6 @@
+export const login = () => {
+  const viewLogin =
+    `<a href="#/">Inicia Sesión</a>
+     <a href="#/signin">Registrate</a>
+    `
+  return viewLogin;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
+import { login } from './lib/view/templateLogin.js';
+import { changeRoute } from './lib/router.js';
 
-myFunction();
+const init = () => {
+  document.getElementById('root').innerHTML = login();
+  window.addEventListener('hashchange', () => {
+    myFunction();
+    console.log(window.location.hash);
+    changeRoute(window.location.hash);
+  });
+};
+window.addEventListener('load', init);
