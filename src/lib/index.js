3eeccdfbe423b918cee02fbb6eca91a5e484cb12
ToroAlejandra <jessicaalejandratc@gmail.com>
2021-08-19 11:07:57 -0500

    Se añaden funciones para registrarse mediante correo y contraseña en firebase

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 // aqui exportaras las funciones que necesites
+let email;
 
-export const loginFirebase = () => {
+export const loginGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth()
 .signInWithPopup(provider)
@@ -27,18 +28,56 @@ export const loginFirebase = () => {
 });
 }
 
+export const sendLink = () => {
+  email = document.querySelector('#inputEmail').value;
+  var actionCodeSettings = {
+    // URL you want to redirect back to. The domain (www.example.com) for this
+    // URL must be in the authorized domains list in the Firebase Console.
+    url: 'http://localhost/?email=' + firebase.auth().currentUser.email,
+    // This must be true.
+    handleCodeInApp: true,
+    /*iOS: {
+      bundleId: 'com.example.ios'
+    },
+    android: {
+      packageName: 'com.example.android',
+      installApp: true,
+      minimumVersion: '12'
+    },*/
+    dynamicLinkDomain: 'localhost:5000'
+  };
 
+  firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
+  .then(() => {
+    // The link was successfully sent. Inform the user.
+    // Save the email locally so you don't need to ask the user for it again
+    // if they open the link on the same device.
+    window.localStorage.setItem('emailForSignIn', email);
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log("errorCode:  ",errorCode,"  errorMessage  ",errorMessage);  
+    // ...
+  });
+};
 
-/*firebase.auth().createUserWithEmailAndPassword(email, password)
-.then((userCredential) => {
-  // Signed in
-  let emailNewUser = document.getElementById('email');
-  var user = userCredential.user;
-  console.log(user);
-  // ...
-})
-.catch((error) => {
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ..
-});*/
+export const signUpWithEmailPassword = () => {
+  email = document.querySelector('#inputEmail').value;
+  let password = document.querySelector('#inputPass').value;
+  // [START auth_signup_password]
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in 
+      var user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ..
+    });
+  // [END auth_signup_password]
+};
+
