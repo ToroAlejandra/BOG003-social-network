6b7c04b7510a36d627e507024acc27531daee0b0
tolozayurany <yalbarracint@gmail.com>
2021-08-20 08:25:03 -0500

    Creación de pruebas para mensajes de error y en index.js modificación de hash por defecto al cargar la página en el main.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-// aqui exportaras las funciones que necesites
+// declarar email como variable gobal
 let email;
 
 export const loginGoogle = () => {
@@ -23,46 +23,12 @@ export const loginGoogle = () => {
   let email = error.email;
   // The firebase.auth.AuthCredential type that was used.
   let credential = error.credential;
-  console.log('error', errorMessage);
+  console.log('errorcredencial', credential, '\n errorMessaage', errorMessage);
   // ...
 });
 }
 
-export const sendLinkOne = () => {
-  email = document.querySelector('#inputEmail').value;
-  let actionCodeSettings = {
-    // URL you want to redirect back to. The domain (www.example.com) for this
-    // URL must be in the authorized domains list in the Firebase Console.
-    url: 'http://localhost/?email=' + firebase.auth().currentUser.email,
-    // This must be true.
-    handleCodeInApp: true,
-    /*iOS: {
-      bundleId: 'com.example.ios'
-    },
-    android: {
-      packageName: 'com.example.android',
-      installApp: true,
-      minimumVersion: '12'
-    },*/
-    dynamicLinkDomain: 'localhost:5000'
-  };
-
-  firebase.auth().sendSignInLinkToEmail(email, actionCodeSettings)
-  .then(() => {
-    // The link was successfully sent. Inform the user.
-    // Save the email locally so you don't need to ask the user for it again
-    // if they open the link on the same device.
-    window.localStorage.setItem('emailForSignIn', email);
-    // ...
-  })
-  .catch((error) => {
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    console.log("errorCode:  ",errorCode,"  errorMessage  ",errorMessage);  
-    // ...
-  });
-};
-
+//enviar link de confirmación al correo
 export const sendLink = () => { 
   // [START auth_send_email_verification] 
   firebase.auth().currentUser.sendEmailVerification() 
@@ -72,6 +38,7 @@ export const sendLink = () => {
   console.log ('...') ;
 }; 
 
+//ingreso con correo y contraseña
 export const signUpWithEmailPassword = () => {
   email = document.querySelector('#inputEmail').value;
   let password = document.querySelector('#inputPass').value;
@@ -80,13 +47,22 @@ export const signUpWithEmailPassword = () => {
     .then((userCredential) => {
       // Signed in 
       let user = userCredential.user;
+      console.log(user);
       // ...
     })
     .catch((error) => {
       let errorCode = error.code;
       let errorMessage = error.message;
+      if (errorCode === 'auth/email-already-in-use'){
+        console.log("entra el if", errorMessage);
+      }
+      else {
+        console.log("eRROR CODE", errorCode);
+      }
       // ..
     });
   // [END auth_signup_password]
 };
 
+//auth/invalid-email
+//auth/weak-password CONTRASEÑA DEBIL
